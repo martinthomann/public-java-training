ea70b6928c96ce1d03a8eadcfda653ee834e7d98
Thomann Martin <martin.thomann@edu.fh-joanneum.at>
2020-08-02 22:59:45 +0200

    a few changes

--- modify-list-strategy/src/main/java/training/Main.java
+++ modify-list-strategy/src/main/java/training/Main.java
@@ -12,16 +12,16 @@ public class Main {
 
         List<Vote> voteList = new ArrayList<Vote>();
 
-        voteList.add(new Vote(new Date(2020, 06,12, 7,55,9), "Tom", false));
-        voteList.add(new Vote(new Date(2020, 06,18, 4,10,8), "TOM", true));
-        voteList.add(new Vote(new Date(2020, 07,11, 12,22,22), "TOM", false));
-        voteList.add(new Vote(new Date(2020, 07,11, 4,45,07), "Mike", true));
-        voteList.add(new Vote(new Date(2020, 06,12, 7,15,59), "Chrissi", true));
-        voteList.add(new Vote(new Date(2020, 07,15, 12,53,58), "ChriSSi", true));
-        voteList.add(new Vote(new Date(2020, 06,30, 22,1,57), "Chrissi", true));
-        voteList.add(new Vote(new Date(2020, 06,17, 11,9,41), "CHRISSI", false));
-        voteList.add(new Vote(new Date(2020, 07,2, 15,21,43), "Elmar", false));
-        voteList.add(new Vote(new Date(2020, 06,25, 3,45,04), "Adi", true));
+        voteList.add(new Vote(new Date(2020, 06, 12, 7, 55, 9), "Tom", false));
+        voteList.add(new Vote(new Date(2020, 06, 18, 4, 10, 8), "TOM", true));
+        voteList.add(new Vote(new Date(2020, 07, 11, 12, 22, 22), "TOM", false));
+        voteList.add(new Vote(new Date(2020, 07, 11, 4, 45, 07), "Mike", true));
+        voteList.add(new Vote(new Date(2020, 06, 12, 7, 15, 59), "Chrissi", true));
+        voteList.add(new Vote(new Date(2020, 07, 15, 12, 53, 58), "ChriSSi", true));
+        voteList.add(new Vote(new Date(2020, 06, 30, 22, 1, 57), "Chrissi", true));
+        voteList.add(new Vote(new Date(2020, 06, 17, 11, 9, 41), "CHRISSI", false));
+        voteList.add(new Vote(new Date(2020, 07, 2, 15, 21, 43), "Elmar", false));
+        voteList.add(new Vote(new Date(2020, 06, 25, 3, 45, 04), "Adi", true));
 
         GuestListGeneratorFactory gF = ServiceLocator.getInstance().newGuestListGeneratorFactory();
         GuestListGenerator gLG = gF.createGuestListGenerator(SET, voteList);
